8925ab4139729981467f2bd2862a6b83e44f4d1d
Larry1997 <larsen.yo@gmail.com>
2017-08-04 14:50:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,12 @@
 console.log('Loaded!');
 
 var img=document.getElementById('rcorner1');
+function moveRight(){
+    marginLeft+=10;
+    img.style.marginLeft= parginLeft + 'px';
+}
 
 img.onclick=function(){
-    img.style.marginLeft ='10px';
+    var interval= setInterval(moveRight,100);
     
 };
