b7f34ae8bf68e0fcae30ed57f66486b10bb6a73a
Larry1997 <larsen.yo@gmail.com>
2017-08-04 14:46:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,7 @@ console.log('Loaded!');
 
 var img=document.getElementById('rcorner1');
 
-img.onclick= function() {
-   document.getElementById('rcorner1').style.position="absolute";
-document.getElementById('rcorner1').style.left="0";
+img.onclick=function(){
+    img.style.marginLeft ='10px';
     
 };
